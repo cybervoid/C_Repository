cf1ccad7098c08fdb43d83c733f57083a35e930e
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-07-06 13:59:26 -0700

    added client.c file for client side communications

--- C_Node_Run/Node/client.c
+++ C_Node_Run/Node/client.c
@@ -26,5 +26,23 @@ int start_client()
         printf("\n socket creation error \n");
         return -1;
     }
-    return;
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    //convert ipv4 and ipv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+    if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection failed \n");
+        return -1;
+    }
+    send(sock, hello, strlen(hello), 0);
+    printf("Hello message sent \n");
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+    return 0;
 }
