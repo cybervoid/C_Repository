12fde4a1f616fbc7a5ac8344e4dfbe7273e6af04
cybervoid <ryan.wemmer@gmail.com>
2018-07-08 10:06:19 -0700

    run_server.sh updated

--- run_server.sh
+++ run_server.sh
@@ -1,8 +1,6 @@
+cd /home/pi/C_Repository
 git fetch
-git reset --hard origin/master
 git pull
-cd ./C_Node_Run/Node
-make -f ./Makefile
-cd ./dist/Debug/GNU-MacOSX
-./node s 3
-cd ~/C_Repository
+cd /home/pi/C_Repository/C_Node_Run/Node/
+make -f /home/pi/C_Repository/C_Node_Run/Node/Makefile
+/home/pi/C_Repository/C_Node_Run/Node/dist/Debug/GNU-MacOSX/node s 3
