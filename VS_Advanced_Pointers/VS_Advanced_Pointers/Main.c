95ba601006fdb342688f1b8796a3376d41b8fd5f
Ryan <cybervoid41@gmail.com>
2018-07-12 11:32:46 -0700

    re-alloc example added

--- VS_Advanced_Pointers/VS_Advanced_Pointers/Main.c
+++ VS_Advanced_Pointers/VS_Advanced_Pointers/Main.c
@@ -1,11 +1,13 @@
 
-//disable compiler warnings: #define _CRT_SECURE_NO_WARNINGS 
+//disable compiler warnings: 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
 /*
 *
 */
+#define _CRT_SECURE_NO_WARNINGS 
 #define LENGTH 3
 #define MAXSTRLEN 100
 int data[LENGTH]; //some integers
@@ -272,9 +274,36 @@ void calloc__big_ex()
 	count += TOTAL;
 }
 
+//changing the size of memory
+void reallocate()
+{
+	char* s;
+	int i;
+
+	i = sizeof("hello");						// as before first work out how big "hello" is ...
+	s = (char*)malloc(i);						// malloc will return 0 if it fails, so we should check here, but we wont for brevity
+	strncpy(s, "hello", i);						// now copy the string into the newly allocated memory
+	printf("s is %s\n", s);
+
+	// now suppose we want to add 'world' to 'hello' - we can't just do this ...
+	// strcat(s, " world");					- disaster!!! 
+	s = (char*)realloc(s, 12);					// but we can use 'realloc' which frees the original 6 bytes of memory and allocates an new 12 bytes
+	strncpy(s, "hello", i);						// now copy the string into the newly re-allocated memory
+	strcat(s, " world");						// now we can tag on the 'world'
+	printf("s is now %s\n", s);
+
+
+	free(s);									// and when we've finished we should always free up any memory by using 'free'
+												// NOTE: make a habit of using 'free' whan you've finished with some memory otherwise there will be 'memory leaks'
+												// where memory that you have allocated will be 'lost' to your program
+												// these are not as important as they used to be (a few bytes in several GB won't make much of a difference) but it is considered to be good programming practise to tidy up
+
+}
+
 int main(int argc, char** argv)
 {
-	calloc__big_ex();
+	reallocate();
+	//calloc__big_ex();
 	//calloc_example();
 	//malloc_and_sizeof();
 	//runtime_memory_allocation();
