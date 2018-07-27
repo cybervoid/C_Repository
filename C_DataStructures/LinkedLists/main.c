4bfaf103588c63f4d3eaa35b433c22968a3b72a9
Ryan <cybervoid41@gmail.com>
2018-07-27 10:18:09 -0700

    linkedlist workspace started

---
+++ C_DataStructures/LinkedLists/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+	int info;	
+	struct node *link;
+};
+
+int main(int argc, char** argv)
+{
+	struct node *start = NULL; //declare a new linked list
+}
