7980138587d0a5024bdee3663ec452231be648c6
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-07-30 08:43:28 -0700

    code added to use linkedlist binary tree

--- C_DataStructures/BinaryTrees/main.c
+++ C_DataStructures/BinaryTrees/main.c
@@ -15,13 +15,21 @@
 #include <stdlib.h>
 
 
+struct node *getnode(char x);
+void display(struct node *p, int level);
 void preorder(struct node *);
 void inorder(struct node *);
 void postorder(struct node *);
-void insertQueue(struct node *);
-int queueEmpty();
+void level_order(struct node *p);
 int height(struct node *);
-struct node *deleteQueue(struct node *);
+
+#define MAX 100
+struct node *queue[MAX];
+int front = -1, rear = -1;
+void insertQueue(struct node *item);
+struct node *deleteQueue();
+int queueEmpty();
+
 struct node
 {
     struct node *lchild;
@@ -35,6 +43,36 @@ struct node
 
 int main(int argc, char** argv) 
 {
+    struct node *root = NULL;
+    
+    root = getnode('P');
+    root->lchild = getnode('Q');
+    root->rchild = getnode('R');
+    root->lchild->lchild = getnode('A');
+    root->lchild->rchild = getnode('B');
+    root->rchild->lchild = getnode('X');
+    
+    display(root, 0);
+    printf("\n\n");
+    
+    printf("Preorder : ");
+    preorder(root);
+    printf("\n\n");
+    
+    printf("inorder : ");
+    inorder(root);
+    printf("\n\n");
+    
+    printf("postorder : ");
+    postorder(root);
+    printf("\n\n");
+
+    printf("level order : ");
+    level_order(root);
+    printf("\n\n");
+    
+    
+    printf("Height of tree is %d\n", height(root));
     return (0);
 }
 
@@ -50,6 +88,27 @@ void preorder(struct node *p)
     preorder(p->rchild);
 }
 
+struct node *getnode(char c)
+{
+    struct node *p;
+    p = (struct node *)malloc(sizeof(struct node));
+    p->info = c;
+    p->lchild = NULL;
+    p->rchild = NULL;
+}
+
+void display(struct node *p, int level)
+{
+    int i;
+    if(p==NULL)
+        return;
+    display(p->rchild, level+1);
+    printf("\n");
+    for(i=0; i<level; i++)
+        printf("    ");
+    printf("%c", p->info);
+    display(p->lchild, level+1);
+}
 //inorder traversal -> l, n, r
 void inorder(struct node *p)
 {
@@ -97,15 +156,15 @@ int height(struct node *p)
 	int hL, hR;
 
 	if (p == NULL)
-		return 0;
+            return 0;
 
 	hL = height(p->lchild);
 	hR = height(p->rchild);
 
 	if (hL > hR)
-		return 1 + hL;
+            return 1 + hL;
 	else
-		return 1 + hR;
+            return 1 + hR;
 }
 void insertQueue(struct node *p) 
 {
