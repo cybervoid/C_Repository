4ceebb8c03f9300a7eb2893fd7b2bc344caad90e
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-07-30 09:10:24 -0700

    added recursive search function for binary search tree

--- C_DataStructures/BinarySearchTrees/main.c
+++ C_DataStructures/BinarySearchTrees/main.c
@@ -10,15 +10,36 @@
  *
  * Created on July 30, 2018, 8:59 AM
  */
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
 
-    return (EXIT_SUCCESS);
+struct node *search(struct node *, int);
+struct node
+{
+    int info;
+    struct node *lchild;
+    struct node *rchild;
+};
+int main() {
+
+    return (0);
 }
 
+struct node *search(struct node *p, int target)
+{
+    if(p == NULL)
+    {
+        printf("%d not found", target);
+        return NULL;
+    }
+    if(p->info == target)
+        return p;
+    
+    if(target < p->info)
+        search(p->lchild, target);
+    
+    if(target > p->info)
+        search(p->rchild, target);
+}
